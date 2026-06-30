e73c7989c8040ceb9516887c03a0938f6e5ac66c
shredderally <zechipsani9@gmail.com>
2026-06-30 04:59:02 +0000

    Update signup.js

--- api/signup.js
+++ api/signup.js
@@ -98,4 +98,4 @@ export default async function handler(req, res) {
     console.error('Signup error:', err.message);
     return res.status(500).json({ error: 'Signup failed. Try again.' });
   }
-                                }
+      }
